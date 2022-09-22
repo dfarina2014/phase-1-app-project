43fd7283153cedf809b1be9c5750a49b236a79ab
dfarina2014 <105017698+dfarina2014@users.noreply.github.com>
2022-09-22 14:04:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-function showAlert() {
+function add() {
     alert("Alert from JS file");
 }
 
 function updateHeading() {
     document.getElementById('heading').innerHTML = 'Heading changed with JS';
 }
+
+document.getElementById("button").style.background='#Blue';
